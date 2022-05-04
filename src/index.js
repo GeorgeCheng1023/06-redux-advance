120c3d51996a704d0c749aaf91edaacfdd6d0933
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-04 11:20:30 +0800

    style: index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
+
 import store from './store/index'
+import {Provider} from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}> <App/> </Provider>);
